af91857cf0ea44aac99f2819ddb8333bcdeee0df
vanneswijaya <vannesoey@gmail.com>
2023-10-26 23:47:02 +0800

    feat: Added barebone SQL migration script and execute in App

--- src/main/java/com/batavia/orm/App.java
+++ src/main/java/com/batavia/orm/App.java
@@ -1,8 +1,13 @@
 package com.batavia.orm;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App 
 {
@@ -11,11 +16,53 @@ public class App
         System.out.println( "Hello World!" );
 
         String url = "jdbc:postgresql://db.jfcjtfntdrivmxlkwgwf.supabase.co:5432/postgres?user=postgres&password=butewahmansion@21";
+
+        Connection connection = null;
+        Statement statement = null;
+
         try {
-            Connection conn = DriverManager.getConnection(url);
+            connection = DriverManager.getConnection(url);
             System.out.println("Connected to the PostgreSQL server successfully.");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            statement = connection.createStatement();
+
+            // Read the migration SQL script from file
+            String migrationScript = readMigrationScript("src/main/java/com/batavia/orm/migration.sql");
+
+            // Execute the migration script
+            statement.executeUpdate(migrationScript);
+
+            System.out.println("Migration executed successfully!");
+
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the statement and connection
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
+    }
+
+    private static String readMigrationScript(String filePath) throws IOException {
+        StringBuilder scriptBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                scriptBuilder.append(line).append(System.lineSeparator());
+            }
+        }
+        return scriptBuilder.toString();
     }
 }
---
+++ src/main/java/com/batavia/orm/migration.sql
@@ -0,0 +1,4 @@
+CREATE TABLE employees (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
