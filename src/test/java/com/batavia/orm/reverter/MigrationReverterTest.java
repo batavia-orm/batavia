4a99106a56436f8236e2718d64ebd485e1b7934c
vanneswijaya <vannesoey@gmail.com>
2023-11-29 00:32:17 +0800

    add test cases for migration reverter with argument

---
+++ src/test/java/com/batavia/orm/reverter/MigrationReverterTest.java
@@ -0,0 +1,106 @@
+package com.batavia.orm.reverter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class MigrationReverterTest {
+    private MigrationReverter migrationReverter;
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String MIGRATIONS_DIR = dotenv.get("MIGRATIONS_DIR");
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        connection = mock(Connection.class);
+        statement = mock(Statement.class);
+        resultSet = mock(ResultSet.class);
+        when(connection.createStatement()).thenReturn(statement);
+        when(statement.executeQuery(anyString())).thenReturn(resultSet);
+        migrationReverter = new MigrationReverter(MIGRATIONS_DIR, connection);
+    }
+
+    @Test
+    void revert_withArgument_shouldRevertMigrationsInReverseOrderUntilDesiredLastAppliedMigration() throws SQLException, IOException {
+        String desiredLastAppliedMigration = "migration3.sql";
+        createMockMigrationFile("migration1.sql", "migration1-content");
+        createMockMigrationFile("migration1.down.sql", "migration1-down-content");
+        createMockMigrationFile("migration2.sql", "migration2-content");
+        createMockMigrationFile("migration2.down.sql", "migration2-down-content");
+        createMockMigrationFile(desiredLastAppliedMigration, "migration3-content");
+
+        // migrationFileExists(desiredLastAppliedMigration) flow
+        when(resultSet.next()).thenReturn(true, false);
+        when(resultSet.getBoolean(1)).thenReturn(true);
+
+        // getMigrationsToRevert flow
+        when(resultSet.next()).thenReturn(true, true, true, false);
+        when(resultSet.getString("migration_file")).thenReturn("migration1.sql", "migration2.sql", "migration3.sql");
+       
+        // revertMigration
+        migrationReverter.revert(desiredLastAppliedMigration);
+
+        // Assert
+        verify(statement).executeQuery("SELECT EXISTS (SELECT * FROM batavia_migrations WHERE migration_file = 'migration3.sql')");
+        verify(statement).executeQuery("SELECT migration_file FROM batavia_migrations ORDER BY id DESC");
+        verify(statement).execute("migration1-down-content");
+        verify(statement).executeUpdate("DELETE FROM batavia_migrations WHERE migration_file = 'migration1.sql'");
+        verify(statement).execute("migration2-down-content");
+        verify(statement).executeUpdate("DELETE FROM batavia_migrations WHERE migration_file = 'migration2.sql'");
+    }
+
+    @Test
+    void revert_withArgument_shouldNotRevertAnyMigrationsIfDesiredLastAppliedMigrationDoesNotExist() throws SQLException, IOException {
+        String desiredLastAppliedMigration = "migration3.sql";
+        createMockMigrationFile("migration1.sql", "migration1-content");
+        createMockMigrationFile("migration1.down.sql", "migration1-down-content");
+        createMockMigrationFile("migration2.sql", "migration2-content");
+        createMockMigrationFile("migration2.down.sql", "migration2-down-content");
+        createMockMigrationFile(desiredLastAppliedMigration, "migration3-content");
+
+        // migrationFileExists(desiredLastAppliedMigration) flow
+        when(resultSet.next()).thenReturn(true, false);
+        when(resultSet.getBoolean(1)).thenReturn(false);
+
+        // revertMigration
+        migrationReverter.revert(desiredLastAppliedMigration);
+
+        // Assert
+        verify(statement).executeQuery("SELECT EXISTS (SELECT * FROM batavia_migrations WHERE migration_file = 'migration3.sql')");
+        verify(statement, never()).executeQuery("SELECT migration_file FROM batavia_migrations ORDER BY id DESC");
+        verify(statement, never()).executeUpdate(anyString());
+        verify(statement, never()).execute(anyString());
+    }
+
+    
+
+    private File createMockMigrationFile(String fileName, String content) throws IOException {
+        String fileContent = content;
+        String filePath = MIGRATIONS_DIR + '/' + fileName;
+
+        File migrationFile = new File(filePath);
+        migrationFile.createNewFile();
+
+        FileWriter fileWriter = new FileWriter(migrationFile);
+        fileWriter.write(fileContent);
+        fileWriter.close();
+
+        migrationFile.deleteOnExit();
+
+        return migrationFile;
+    }
+}
