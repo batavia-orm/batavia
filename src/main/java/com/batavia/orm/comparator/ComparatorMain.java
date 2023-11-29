8ec685513319a661cb95cd8a0bc6b3d1524d83c8
enryleinhard <42536568+enryleinhard@users.noreply.github.com>
2023-11-30 03:46:51 +0800

    merge

--- src/main/java/com/batavia/orm/comparator/ComparatorMain.java
+++ src/main/java/com/batavia/orm/comparator/ComparatorMain.java
@@ -1,44 +1,37 @@
 package com.batavia.orm.comparator;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.batavia.orm.adapters.Database;
 import com.batavia.orm.commons.Column;
 import com.batavia.orm.commons.Table;
 import com.batavia.orm.generator.GeneratorMain;
 import com.batavia.orm.generator.SqlCommandContext;
 import com.batavia.orm.generator.sqlScriptGenerators.AlterTableContext;
+import com.batavia.orm.scanner.DataSourceScanner;
+import com.batavia.orm.scanner.DatabaseScanner;
 
 public class ComparatorMain {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException, IOException {
     System.out.println("Hello World!");
-    HashMap<String, Table> dbTable = new HashMap<String, Table>();
-    HashMap<String, Table> localTable = new HashMap<String, Table>();
+    Database dbInstance = Database.getDatabase();
 
-    Table thisislocal = new Table("table1");
-    thisislocal.addColumn(new Column("column1", "varchar", true, true));
-    thisislocal.addColumn(new Column("column3", "varchar", false, false));
+    DataSourceScanner dataSourceScanner = new DataSourceScanner("/Users/enryleinhard/Projects/eclipse/exampleProject/src");
+    DatabaseScanner databaseScanner = new DatabaseScanner(dbInstance);
 
-    Table thisisremote = new Table("table1");
-    thisisremote.addColumn(new Column("column1", "varchar", true, true));
-    thisisremote.addColumn(new Column("column2", "varchar", false, false));
-    thisisremote.addColumn(new Column("column3", "varchar", true, false));
-    
-    Table thisislocal2 = new Table("table2");
-    Table thisisremote2 = new Table("table69");
-    localTable.put("table1", thisislocal);
-    localTable.put("table2", thisislocal2);
+    HashMap<String, Table> databaseTables = databaseScanner.findAllTables();
+    HashMap<String, Table> dataSourceTables = dataSourceScanner.findAllEntities();
 
-    dbTable.put("table1", thisisremote);
-    dbTable.put("table69", thisisremote2);
-    
-    ComparatorMain.comp(localTable, dbTable);
+    ComparatorMain.comp(dataSourceTables, databaseTables);
 
   }
   
   public static void comp(HashMap<String, Table> local, HashMap<String, Table> remote) {
-    String upSQLFile = "/home/dannel/Documents/Dannel/Project/batavia/src/main/java/com/batavia/orm/comparator/scripts/test1.sql";
-    String downSQLFile = "/home/dannel/Documents/Dannel/Project/batavia/src/main/java/com/batavia/orm/comparator/scripts/test2.sql";
+    String upSQLFile = "/Users/enryleinhard/Projects/batavia/src/main/java/com/batavia/orm/comparator/up.sql";
+    String downSQLFile = "/Users/enryleinhard/Projects/batavia/src/main/java/com/batavia/orm/comparator/down.sql";
     local.keySet().forEach(localTableName -> {
       if (remote.containsKey(localTableName)) {
         // Table exists in both local and remote
@@ -75,24 +68,37 @@ public class ComparatorMain {
           upSQLFile,
           downSQLFile
         );
-        gen2.runSqlScriptGeneratorToFile(
-          SqlCommandContext.ALTER_TABLE,
-          AlterTableContext.ADD_COLUMN
-        );
+        try {
+          gen2.runSqlScriptGeneratorToFile(
+            SqlCommandContext.ALTER_TABLE,
+            AlterTableContext.ADD_COLUMN
+          );
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
 
       } else {
         // Table exists in local but not in remote
         // Create table in remote
         //OK
+        local.get(localTableName).getColumns().forEach((k, v) -> {
+          System.out.println("create column " + k + " in remote");
+        });
         GeneratorMain gen1 = new GeneratorMain(
           local.get(localTableName),
           upSQLFile,
           downSQLFile
         );
-        gen1.runSqlScriptGeneratorToFile(
-          SqlCommandContext.CREATE_TABLE,
-          null
-        );
+        try {
+          gen1.runSqlScriptGeneratorToFile(
+            SqlCommandContext.CREATE_TABLE,
+            AlterTableContext.NONE
+          );
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
         System.out.println("create table " + localTableName + " in remote");
       }
     });
@@ -113,7 +119,7 @@ public class ComparatorMain {
             Column remoteColumn = remoteTable.getColumns().get(remoteColumnName);
 
             if (!localColumn.getColumnType().equals(remoteColumn.getColumnType())) {
-              dropColumns.add()
+
               System.out.println("Column " + remoteColumnName + " in table " + remoteTableName + " has different type, it should be " + localColumn.getColumnType() + " instead ");
             }
             if (!localColumn.isPrimary().equals(remoteColumn.isPrimary())) {
@@ -136,10 +142,15 @@ public class ComparatorMain {
           upSQLFile,
           downSQLFile
         );
-        gen3.runSqlScriptGeneratorToFile(
-          SqlCommandContext.ALTER_TABLE,
-          AlterTableContext.DROP_COLUMN
-        );
+        try {
+          gen3.runSqlScriptGeneratorToFile(
+            SqlCommandContext.ALTER_TABLE,
+            AlterTableContext.DROP_COLUMN
+          );
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
       } else {
         // Table exists in remote but not in local
         // Drop table in remote
@@ -148,10 +159,15 @@ public class ComparatorMain {
           upSQLFile,
           downSQLFile
         );
-        gen3.runSqlScriptGeneratorToFile(
-          SqlCommandContext.DROP_TABLE,
-          null
-        );
+        try {
+          gen3.runSqlScriptGeneratorToFile(
+            SqlCommandContext.DROP_TABLE,
+            AlterTableContext.NONE
+          );
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
         System.out.println("drop table " + remoteTableName + " in remote");
       }
     });
