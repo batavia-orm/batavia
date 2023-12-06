d90694de11c2baac4a95cd4b1fff780abeb352e4
enryleinhard <42536568+enryleinhard@users.noreply.github.com>
2023-12-06 23:30:28 +0800

    feat: add comparator test add and drop column

--- src/test/java/com/batavia/orm/comparator/ComparatorTest.java
+++ src/test/java/com/batavia/orm/comparator/ComparatorTest.java
@@ -10,7 +10,17 @@ import java.util.HashMap;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import com.batavia.orm.commons.Column;
 import com.batavia.orm.commons.Table;
+
+
+    // Comparator comparator = new Comparator("/Users/enryleinhard/Projects/eclipse/exampleProject/src/migrations", ".");
+    // comparator.comp(localTables, remoteTables, "comparatorTest");
+
+
+    // String upSQLContent = Files.readString(Path.of("/Users/enryleinhard/Projects/eclipse/exampleProject/src/migrations/comparatorTest.sql"));
+    // String downSQLContent = Files.readString(Path.of("/Users/enryleinhard/Projects/eclipse/exampleProject/src/migrations/comparatorTest.down.sql"));
+
 
 public class ComparatorTest {
   @Test
@@ -63,5 +73,62 @@ public class ComparatorTest {
 
     assertEquals(upSQLContent, "DROP TABLE table_b;\n\n");
     assertEquals(downSQLContent, "CREATE TABLE table_b (\n\n);\n\n");
+  }
+
+  @Test
+  public void Should_Generate_Create_Column_When_Local_Exists_Remote_Not_Exists(@TempDir Path tempDir) throws IOException {
+    Path upSQLPath = Files.createFile(tempDir.resolve("comparatorTest.sql"));
+    Path downSQLPath = Files.createFile(tempDir.resolve("comparatorTest.down.sql"));
+
+    HashMap<String, Table> localTables = new HashMap<String, Table>();
+    HashMap<String, Table> remoteTables = new HashMap<String, Table>();
+
+    Table tableA = new Table("table_a");
+    Column columnA = new Column("col_a", "String");
+    tableA.addColumn(columnA);
+
+    Table remoteTableA = new Table("table_a");
+
+    localTables.put("table_a", tableA);
+    remoteTables.put("table_a", remoteTableA);
+
+    Comparator comparator = new Comparator(tempDir.toString(), ".");
+    comparator.comp(localTables, remoteTables, "comparatorTest");
+
+
+    String upSQLContent = Files.readString(upSQLPath);
+    String downSQLContent = Files.readString(downSQLPath);
+
+    assertEquals(upSQLContent, "ALTER TABLE table_a\n" + "ADD COLUMN col_a VARCHAR;\n\n");
+    assertEquals(downSQLContent, "ALTER TABLE table_a\n" + "DROP COLUMN col_a;\n\n");
+  }
+
+  @Test
+  public void Should_Generate_Drop_Column_When_Local_Not_Exists_Remote_Exists(@TempDir Path tempDir) throws IOException {
+    Path upSQLPath = Files.createFile(tempDir.resolve("comparatorTest.sql"));
+    Path downSQLPath = Files.createFile(tempDir.resolve("comparatorTest.down.sql"));
+
+    HashMap<String, Table> localTables = new HashMap<String, Table>();
+    HashMap<String, Table> remoteTables = new HashMap<String, Table>();
+
+    Table tableA = new Table("table_a");
+
+    Table remoteTableA = new Table("table_a");
+    Column columnA = new Column("col_a", "String");
+    remoteTableA.addColumn(columnA);
+
+
+    localTables.put("table_a", tableA);
+    remoteTables.put("table_a", remoteTableA);
+
+    Comparator comparator = new Comparator(tempDir.toString(), ".");
+    comparator.comp(localTables, remoteTables, "comparatorTest");
+
+
+    String upSQLContent = Files.readString(upSQLPath);
+    String downSQLContent = Files.readString(downSQLPath);
+
+    assertEquals(upSQLContent, "ALTER TABLE table_a\n" + "DROP COLUMN col_a;\n\n");
+    assertEquals(downSQLContent, "ALTER TABLE table_a\n" + "ADD COLUMN col_a VARCHAR;\n\n");
   }
 }
