99e62bfafd9c43ee0e014f340b0296a1d2359c54
dannelmlj <dannelmuljaa@gmail.com>
2023-12-06 15:19:22 +0800

    test: add Scanner test for findTable and findTables()

--- src/test/java/com/batavia/orm/scanner/DataBaseScannerTest.java
+++ src/test/java/com/batavia/orm/scanner/DataBaseScannerTest.java
@@ -1,71 +1,114 @@
 package com.batavia.orm.scanner;
 
+import static org.junit.Assert.assertEquals;
 
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.mockito.Mockito.*;
 
+import com.batavia.orm.adapters.Database;
 import com.batavia.orm.commons.*;
 
 public class DataBaseScannerTest {
   private ResultSet resultSet;
   private ResultSet resultSet2;
   private DatabaseMetaData dbMetadata;
+  private Database dbInstance;
+  private Column cols;
+  private Table table;
 
   @BeforeEach
   void setUp() throws SQLException {
     resultSet = mock(ResultSet.class);
     resultSet2 = mock(ResultSet.class);
     dbMetadata = mock(DatabaseMetaData.class);
+    dbInstance= mock(Database.class);
+    cols = mock(Column.class);
+    table = mock(Table.class);
+    String colsName = "name";
+    String typeName = "varchar";
+    String tableName = "tabName";
+
+    when(cols.getColumnName()).thenReturn("name");
+    when(cols.getColumnType()).thenReturn("varchar");
+    when(resultSet.getString("COLUMN_NAME")).thenReturn(colsName);
+    when(resultSet.getString("TABLE_NAME")).thenReturn(tableName);
+    when(resultSet.getString("TYPE_NAME")).thenReturn(typeName);
+    when(resultSet2.getString("COLUMN_NAME")).thenReturn(colsName);
+    when(resultSet2.getString("TYPE_NAME")).thenReturn(typeName);
+
+
+    when(dbMetadata.getPrimaryKeys(null, null, tableName)).thenReturn(resultSet);
+    when(dbMetadata.getColumns(null, null, tableName, null)).thenReturn(resultSet2);
+    when(resultSet.next()).thenReturn(true).thenReturn(false);
+    when(resultSet2.next()).thenReturn(true).thenReturn(false);
+    when(dbInstance.getMetadata()).thenReturn(dbMetadata);
+ 
+    
+
   }
   @Test
   public void Should_Correctly_Find_The_Columns_Of_A_Table() throws SQLException {
     // Arrange
     Column columnResult;
     String primaryKey = "id";
-    DatabaseScanner databaseScanner = new DatabaseScanner(null);
+    DatabaseScanner databaseScanner = new DatabaseScanner(dbInstance);
     
     when(resultSet.getString("COLUMN_NAME")).thenReturn("name");
     when(resultSet.getString("TYPE_NAME")).thenReturn("varchar");
     // Act
     columnResult = databaseScanner.findColumn(resultSet, primaryKey);
-    // Assert
-    assert(columnResult.getColumnName().equals("name"));
-    assert(columnResult.getColumnType().equals("varchar"));
-    assert(columnResult.isPrimary().equals(false));
-    assert(columnResult.isUnique().equals(false));
+    // assertEquals
+    assertEquals(columnResult.getColumnName(), "name");
+    assertEquals(columnResult.getColumnType(), "VARCHAR");
+    assertEquals(columnResult.isPrimary(), false);
+    assertEquals(columnResult.isUnique(), false);
   }
 
   @Test
   public void Should_Correctly_Find_The_Primary_Key_Of_A_Table() throws SQLException {
     // Arrange
     String primaryKeyResult;
-    DatabaseScanner databaseScanner = new DatabaseScanner(null);
+    DatabaseScanner databaseScanner = new DatabaseScanner(dbInstance);
     when(resultSet.getString("COLUMN_NAME")).thenReturn("id");
     // Act
     primaryKeyResult = databaseScanner.findPrimaryKey(resultSet);
-    // Assert
-    assert(primaryKeyResult.equals("id"));
+    // assertEquals
+    assertEquals(primaryKeyResult, "id");
   }
 
   @Test
   public void Should_Correctly_Find_A_Table() throws SQLException {
     // Arrange
     String tableName = "example_table";
-    DatabaseScanner databaseScanner = new DatabaseScanner(null);
-    when(resultSet.next()).thenReturn(true).thenReturn(false);
-    when(resultSet2.next()).thenReturn(true).thenReturn(false);
-    when(dbMetadata.getColumns(null, null, tableName, null)).thenReturn(resultSet);
-    when(dbMetadata.getPrimaryKeys(null, null, tableName)).thenReturn(resultSet2);
+    DatabaseScanner databaseScanner = new DatabaseScanner(dbInstance);
+    Table tableResult;
+    when(dbMetadata.getPrimaryKeys(null, null, tableName)).thenReturn(resultSet);
+    when(dbMetadata.getColumns(null, null, tableName, null)).thenReturn(resultSet2);
+    when(databaseScanner.findPrimaryKey(resultSet)).thenReturn("id");
     // Act
-    Table tableResult = databaseScanner.findTable(tableName);
-    // Assert
-    assert(tableResult.getTableName().equals("example_table"));
+    tableResult = databaseScanner.findTable(tableName);
+    // assertEquals
+    assertEquals(tableResult.getTableName(), "example_table");
 
+  }
+
+  @Test
+  public void Should_Correctly_Find_Tables() throws SQLException {
+    // Arrange 
+    String tableName= "tabName";
+    DatabaseScanner databaseScanner = new DatabaseScanner(dbInstance);
+    HashMap<String, Table> hashTables;
+    when(databaseScanner.findPrimaryKey(resultSet)).thenReturn("id");
+    // Act
+    hashTables = databaseScanner.findTables(resultSet);
+    // Assert
+    assertEquals(hashTables.get(tableName).getTableName(), "tabName" );
   }
 }
