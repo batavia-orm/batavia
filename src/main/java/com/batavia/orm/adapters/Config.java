893afc3ecd8c2d62479f7191696c1a2a4e00a73e
enryleinhard <42536568+enryleinhard@users.noreply.github.com>
2023-12-09 18:14:22 +0800

    feat: add migrations and datasource dir in config

--- src/main/java/com/batavia/orm/adapters/Config.java
+++ src/main/java/com/batavia/orm/adapters/Config.java
@@ -6,10 +6,14 @@ public class Config {
 
   private static Config configInstance;
   private String databaseURL;
+  private String migrationsDir;
+  private String dataSourceDir;
 
   private Config() {
     Dotenv dotenv = Dotenv.load();
     this.databaseURL = dotenv.get("DATABASE_URL");
+    this.migrationsDir = dotenv.get("MIGRATIONS_DIR");
+    this.dataSourceDir = dotenv.get("DATASOURCE_DIR");
   }
   
   public static Config getConfig() {
@@ -21,5 +25,13 @@ public class Config {
 
   public String getDatabaseURL() {
     return this.databaseURL;
+  }
+
+  public String getMigrationsDir() {
+    return this.migrationsDir;
+  }
+
+  public String getDataSourceDir() {
+    return this.dataSourceDir;
   }
 }
